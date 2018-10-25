250c6bb1e70f12c865e86e51f0a46ff2289ca5c2
Cujoko <cujoko@gmail.com>
2018-10-26 05:25:35 +0900

    Изменит ссылку на проект в setup.py c Github на Gitlab.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Diff utility for 1C:Enterprise',
     author='Cujoko',
     author_email='cujoko@gmail.com',
-    url='https://github.com/Cujoko/diff-1c',
+    url='https://gitlab.com/Cujoko/diff-1c',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
