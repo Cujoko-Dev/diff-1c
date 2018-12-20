df498d0d9b2191f7ff4d41d317589c2b7757032e
Cujoko <cujoko@gmail.com>
2018-12-20 16:22:08 +0900

    Рефакторинг

--- diff_1c/__init__.py
+++ diff_1c/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+import logging
+
 # noinspection PyUnresolvedReferences
 from diff_1c.__about__ import __version__
 # noinspection PyUnresolvedReferences
 from diff_1c.main import Processor
-import logging
 
 # noinspection PyUnresolvedReferences
 logging.getLogger().setLevel(logging.DEBUG)
