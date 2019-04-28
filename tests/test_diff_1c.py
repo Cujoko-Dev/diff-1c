9050dee18287ae9156983ccc4a0b93fe0b75aeb5
Cujoko <cujoko@gmail.com>
2019-04-28 19:44:13 +0900

    Улучшит тесты

--- tests/test_diff_1c.py
+++ tests/test_diff_1c.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 
 from diff_1c.cli import get_argparser
 from diff_1c.main import run
 
 
-class MainTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.parser = get_argparser()
+@pytest.fixture()
+def test():
+    parser = get_argparser()
 
-    def test_run(self) -> None:
-        args = self.parser.parse_args('data/test.epf data/test.epf'.split())
-        run(args)
+    return parser
+
+
+def test_run(test):
+    parser = test
+
+    args = parser.parse_args('data/test.epf data/test.epf'.split())
+    run(args)
